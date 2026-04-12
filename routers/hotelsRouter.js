f09f1050c663bb6dff2fb28dc997bc4e3c1ac6d4
elankathir <esekar@diligentsquad.com>
2026-04-12 10:45:25 +0530

    claculate average using match group and  sort

--- routers/hotelsRouter.js
+++ routers/hotelsRouter.js
@@ -6,6 +6,9 @@ const hotelRouter = express.Router();
 //Route Chaining
 hotelRouter.route('/get-featured')
 .get(hotelsController.getFeaturedHotels, hotelsController.getAll);
+hotelRouter.route('/get-hotel-stats')
+.get(hotelsController.getHotelStats);
+
 hotelRouter.route('/')
 .get(hotelsController.getAll)
 .post(hotelsController.create)
