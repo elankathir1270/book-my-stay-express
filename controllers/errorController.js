6d1d9ba411260f113646a7441d77af2bc68f795f
elankathir <esekar@diligentsquad.com>
2026-04-17 12:30:20 +0530

    handle development and production error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,38 @@
+const devErrors = (res,error) => {
+
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message,
+            stackTrace: error.stackTrace,
+            error: error
+        })
+}
+
+const prodErrors = (res,error) => {
+    if(error.isOperational) {
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message
+        })
+    }else{
+        res.status(error.statusCode).json({
+            status: 'error',
+            message: 'Something went wrong please try again later'
+        })
+    }
+
+}
+
+
 module.exports = (error,req,res,next) => {
+
     const statusCode = error.statusCode || 500;
     const status = error.status || "Error";
 
-    res.status(statusCode).json({
-        status,
-        message: error.message
-    })
+    if(process.env.NODE_ENV === 'development'){
+        devErrors(res, error);
+    }else{
+        prodErrors(res,error);
+    }
+
 }
