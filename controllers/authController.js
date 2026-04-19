a2382f462896e2f1ed2edb3ffe7f8ab02a64f28f
elankathir <esekar@diligentsquad.com>
2026-04-19 12:55:02 +0530

    create jwt token and auto login after signup with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,16 @@
 const catchAsync = require('../utilities/catchAsync');
 const User = require('../model/user');
+const jwt = require('jsonwebtoken');
 
 
 exports.signup = catchAsync(async (req,res,next) => {
-    const newUser = await User.create(req.body);    
+    const newUser = await User.create(req.body);
+    
+    const token = jwt.sign({ userId: newUser._id }, process.env.SECRET_KET,{expiresIn : process.env.LOGIN_EXPIRES});
 
     res.status(201).json({
         status: "success",
+        token,
         data: newUser
     })
 })
