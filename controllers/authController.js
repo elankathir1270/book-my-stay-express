2111fcc3e1016cf34e3be5b851fc0ab6df35c6ff
elankathir <esekar@diligentsquad.com>
2026-04-19 13:53:40 +0530

    login - validate credentials and find user to validate

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const catchAsync = require('../utilities/catchAsync');
 const User = require('../model/user');
 const jwt = require('jsonwebtoken');
+const AppError = require('../utilities/appError');
 
 
 exports.signup = catchAsync(async (req,res,next) => {
@@ -12,5 +13,29 @@ exports.signup = catchAsync(async (req,res,next) => {
         status: "success",
         token,
         data: newUser
+    })
+})
+
+exports.login = catchAsync(async (req,res,next) => {
+    const { email,password } = req.body;
+
+    if(!email || email === ' '){
+        const error = new AppError('Email is not provided', 400);
+        return next(error);
+    }
+    if(!password || password === ' '){
+        const error = new AppError('Password is not provided', 400);
+        return next(error);
+    }
+    //Check if user with given email exist 
+    const user = await User.findOne({email: email});
+    if(!user){
+        const error = new AppError("User with given email is not found", 400);
+        return next(error);
+    }
+
+    res.status(200).json({
+        status: "success",
+        token: "tokrn"
     })
 })
